9ac825a7a5bf6f2cecaa9c74966dad66eb5c1f9f
kiritoxkiriko <wangxinmeng1997@hotmail.com>
2025-01-21 16:19:52 +0800

    build: bump img to 0.4.1.post7

--- python/sglang/srt/entrypoints/http_server.py
+++ python/sglang/srt/entrypoints/http_server.py
@@ -93,8 +93,11 @@ app.add_middleware(
 
 # body logger
 import time
+ENABLE_LOG_REQUESTS = os.environ.get('ENABLE_LOG_REQUESTS', '0') == '1'
 
 def body_logger(raw_request: Request, start_time: float, resp=None):
+    if not ENABLE_LOG_REQUESTS:
+        return
     request_body = raw_request.json()
     if resp is not None and hasattr(resp, 'model_dump_json'):
         resp = resp.model_dump_json()
